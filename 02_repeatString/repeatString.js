b75cabd45723325494028073fa01281da6aadf73
ares1345 <ares1345istaken@gmail.com>
2023-04-21 22:06:19 +0200

    Finish 1st exercice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,4 @@
 const repeatString = function(string, num) {
-    // let i = 0;
-    // let result;
-    // while(i <= num) {
-    //     result += string;
-    //     i++ ;
-    // }
-    // return result
     if (num == 1) {
         return string
     }
@@ -16,8 +9,7 @@ const repeatString = function(string, num) {
         return 'ERROR'
     }
 
-
-
+    
     let repeatedString = string;
     for (let i = 1; i <= num-1; i++) {
 
@@ -25,10 +17,6 @@ const repeatString = function(string, num) {
     }
      return repeatedString;
 };
-
-repeatString('hey', 3);
-
-repeatString('hey', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
