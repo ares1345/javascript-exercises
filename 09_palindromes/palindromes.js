27b2930650c95bcbda4cb1ae2f524afb2d173c3d
ares1345 <ares1345istaken@gmail.com>
2023-04-24 18:03:01 +0200

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toUpperCase(); 
+    string = string.replace(/[^a-zA-Z0-9]/g, '');
+    string = string.split('');
+    let oldString = string.join('');
+    //for some reason, string was also reversed in the process??
+    let revString = string.reverse();
+    oldString = oldString.split('')
+    for(i = 0; i <= (string.length - 1); i++){
+        
+        if (!(oldString[i] === revString[i])){
+            return false;
+            
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
