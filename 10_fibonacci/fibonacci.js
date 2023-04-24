9f4862374f340aef19ad7b2c3db009aa2c1d97b8
ares1345 <ares1345istaken@gmail.com>
2023-04-24 18:23:16 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a <= 0) return 'OOPS';
 
+    a = parseInt(a);
+    oldNumber = 1;
+    newNumber = 1;
+    for(i=1; i <= (a - 2); i++){
+        let temp = newNumber // 1 2
+        newNumber+=oldNumber // 2 3
+        oldNumber = temp // 1
+    }
+    return newNumber
 };
 
 // Do not edit below this line
