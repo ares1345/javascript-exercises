ab4eb1accffdb16f5f64ddb5943bbe4b89723f5a
ares1345 <ares1345istaken@gmail.com>
2023-04-24 14:43:05 +0200

    Finish 3rd and 4th exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for(let f in args) {
+        for(let i = 0; i <= array.length; i++) {
+            if (array[i] === args[f]){
+                array.splice(array.indexOf(array[i]), 1);
+            }
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
