ecad1f48d6eaf84fa4fae030c9b1fb8364208537
ares1345 <ares1345istaken@gmail.com>
2023-04-24 15:15:46 +0200

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if(a > b){
+        let sum = 0;
+        for(i = b; i<=a; i++){
+            sum += i
+        }
+        return sum 
+    }
+
+    else if(a < 0 || b < 0){
+        return 'ERROR'
+    }
+
+
+
+    else if ((typeof a === "number") && (typeof b === "number")) {
+        let sum = 0;
+        for(i = a; i<=b; i++){
+            sum += i
+        }
+        return sum
+    } else {
+        return 'ERROR'
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
