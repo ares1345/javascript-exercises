7334aaa163e43b5f8865e3f50b2cfa31c048d883
ares1345 <ares1345istaken@gmail.com>
2023-04-24 16:24:55 +0200

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+	for(let i = 0; i<= (a.length - 1); i++) {
+    total += a[i]
+  }
+  
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let res = 1;
+  for(i = 0; i<=(a.length) - 1; i++){
+    res *= a[i];
+  }
+  return res
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a <= 1){
+    return 1;
+  }
+  let res = 1
+	for(let i = a; i>=1; i--){
+    res *= i;
+  }
+  return res; 
 };
 
 // Do not edit below this line
